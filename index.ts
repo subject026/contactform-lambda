e7fdc7e97782e2ef47cfb49ccff5335e4541855c
subject026 <subject026@protonmail.com>
2022-09-19 20:14:41 +0100

    add some validation

--- index.ts
+++ index.ts
@@ -1,10 +1,35 @@
+import z from "zod";
+
+const FormDTO = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  message: z.string(),
+});
+
+// type TFormDTO = z.infer<typeof FormDTO>;
+
 export const handler = async (event: any) => {
-  return {
-    statusCode: 200,
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-    },
-    body: JSON.stringify({ contact: "form" }),
-  };
+  try {
+    const formData = FormDTO.parse(event);
+    console.log("validated formdata: ", formData);
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({ message: "nicccce" }),
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      statusCode: 418,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({ error: err }),
+    };
+  }
 };
