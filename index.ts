682850cf6d414b7e465306dc87b1201e192cde10
subject026 <subject026@protonmail.com>
2022-09-19 19:13:00 +0100

    add some headers

--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
 export const handler = async (event: any) => {
   return {
     statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+    },
     body: JSON.stringify({ contact: "form" }),
   };
 };
